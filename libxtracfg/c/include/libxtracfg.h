326f2486ee29c57a23d742504897f97821583016
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-10-11 13:56:44 +0200

    fix c header

--- libxtracfg/c/include/libxtracfg.h
+++ libxtracfg/c/include/libxtracfg.h
@@ -1,1 +1,8 @@
-char* xtracfg_execute(const char* command, int* err);
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+    char* xtracfg_execute(const char* command, int* err);
+#ifdef __cplusplus
+}
+#endif
