bc40c1c616585d31d23a0ea67e3da4408a128c5f
PZahnen <pascal.zahnen@gmx.de>
2024-10-15 18:12:13 +0200

    correction

--- libxtracfg/js/lib/binding.js
+++ libxtracfg/js/lib/binding.js
@@ -1,4 +1,4 @@
-const addon = require("../build/Release/xtracfg-lib-native");
+const addon = require("/Users/pascal/Documents/GitHub/xtraplatform-cli/libxtracfg/js/build/Release/xtracfg-lib-native.node");
 
 module.exports = {
   xtracfgLib: addon.xtracfgLib,
