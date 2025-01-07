148ae607323c1414894c2fbcf900207e142cb9b3
Andreas Zahnen <zahnen@interactive-instruments.de>
2025-01-07 14:32:27 +0100

    refactor

--- libxtracfg/.ts2x/index.ts
+++ libxtracfg/.ts2x/index.ts
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 
-import { generate } from "./ts2x/index.ts";
+import { generate } from "ts2x/src/index.ts";
 import { onClass, onNamespace, getAdditional } from "./java.ts";
 
 //TODO: restructure -> xtracfg/bin, xtracfg/lib/*, gen in root?
@@ -9,15 +9,15 @@ generate({
   source: resolve("./src/index.ts"),
   verbose: false,
   schema: {
-    basePath: "./src/gen",
+    basePath: "../js/xtracfg/src/gen",
   },
   go: {
-    basePath: "../../go",
+    basePath: "../go",
     pkg: "gen",
     filePrefixes: { Command: "cmd-", Options: "opts-", Result: "res-" },
   },
   java: {
-    basePath: "../../java/src/main/java",
+    basePath: "../java/src/main/java",
     pkg: "de.ii.xtraplatform.cli.gen",
     classSuffixes: ["Command", "Options", "Result"],
     additionalClasses: getAdditional("de.ii.xtraplatform.cli.gen"),
@@ -27,6 +27,6 @@ generate({
     },
   },
   ts: {
-    basePath: "./src/gen",
+    basePath: "../js/xtracfg/src/gen",
   },
 });
