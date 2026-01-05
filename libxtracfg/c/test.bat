22d68ce5eaa98e7939ab279312a23242d0aaec2d
Andreas Zahnen <zahnen@interactive-instruments.de>
2026-01-05 20:09:38 +0100

    windows build

--- libxtracfg/c/test.bat
+++ libxtracfg/c/test.bat
@@ -2,6 +2,7 @@ REM @echo off
 
 set DIR=%~dp0
 set DIR=%DIR:~0,-1%
+set DIR=D:\\a\\xtraplatform-cli\\xtraplatform-cli\\libxtracfg\\c
 
 cp C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1/bin/server\jvm.dll build\
 
