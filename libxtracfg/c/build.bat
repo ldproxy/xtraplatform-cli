26c7c18b42353a19c5444c1338df031398ccfc0d
Andreas Zahnen <zahnen@interactive-instruments.de>
2026-01-05 19:02:39 +0100

    windows build

--- libxtracfg/c/build.bat
+++ libxtracfg/c/build.bat
@@ -32,34 +32,10 @@ cl.exe /MT /I./ /Fetest.exe /I%JAVA_HOME%/include /I%JAVA_HOME%/include/%PLATFOR
  ../wrapper/libxtracfg.c ^
  ../test/main.c ^
  libxtracfgjni_static.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\svm\clibraries\windows-amd64\libchelper.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\net.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\extnet.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\nio.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\management_ext.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\java.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\sunmscapi.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\static\windows-amd64\zip.lib ^
- C:\hostedtoolcache\windows\graalvm-jdk-21_windows-x64_bin\21.0.0\x64\graalvm-jdk-21.0.9+7.1\lib\svm\clibraries\windows-amd64\jvm.lib ^
  /link ^
   /FILEALIGN:4096 ^
   /LIBPATH:./ ^
-  /LIBPATH:%JAVA_HOME%/lib ^
-  /LIBPATH:%JAVA_HOME%/lib/svm/clibraries ^
-  /LIBPATH:%JAVA_HOME%/lib/svm/clibraries/windows-amd64 ^
-  /LIBPATH:%JAVA_HOME%/lib/static/windows-amd64 ^
-  /WHOLEARCHIVE:libxtracfgjni_static.lib ^
-  ncrypt.lib ^
-  crypt32.lib ^
-  winhttp.lib ^
-  psapi.lib ^
-  version.lib ^
-  advapi32.lib ^
-  ws2_32.lib ^
-  secur32.lib ^
-  iphlpapi.lib ^
-  userenv.lib ^
-  mswsock.lib
+  /WHOLEARCHIVE:libxtracfgjni_static.lib
 REM /verbose
 REM %JAVA_HOME%/lib/jvm.lib
 
